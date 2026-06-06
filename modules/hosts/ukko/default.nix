a6a58227d85a801f87ece69e6edd0a34aef53483
Sami Reunanen <sami@reuski.dev>
2026-06-06 18:12:38 +0300

    bleeding edge

--- modules/hosts/ukko/default.nix
+++ modules/hosts/ukko/default.nix
@@ -3,29 +3,31 @@ let
   inherit (config.flake.modules) nixos;
 in
 {
-  configurations.nixos.ukko.module = {
-    imports = [
-      inputs.disko.nixosModules.disko
-      ./_disko.nix
-      ./_hardware.nix
-      ./_network.nix
-      ./_services.nix
-      nixos.boot
-      nixos.server
-      nixos.metal
-      nixos.quadlets
-      nixos.heimdash
-      nixos.media
-      nixos.jellyfin
-      nixos.audiobookshelf
-      nixos.maintainerr
-      nixos.servarr
-      nixos.qbittorrent
-      nixos.hass
-      nixos.vaultwarden
-      nixos.skaldi
-    ];
+  configurations.nixos.ukko.module =
+    { config, ... }:
+    {
+      imports = [
+        inputs.disko.nixosModules.disko
+        ./_disko.nix
+        ./_hardware.nix
+        ./_network.nix
+        ./_services.nix
+        nixos.boot
+        nixos.server
+        nixos.metal
+        nixos.quadlets
+        nixos.heimdash
+        nixos.media
+        nixos.jellyfin
+        nixos.audiobookshelf
+        nixos.maintainerr
+        nixos.servarr
+        nixos.qbittorrent
+        nixos.hass
+        nixos.vaultwarden
+        nixos.skaldi
+      ];
 
-    system.stateVersion = "25.11";
-  };
+      system.stateVersion = config.system.nixos.release;
+    };
 }
