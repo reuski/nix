241d50fc198dab0027cb0071f26f274b37bf0906
Sami Reunanen <sami@reuski.dev>
2026-06-09 07:49:12 +0300

    fix noctalia

--- flake.nix
+++ flake.nix
@@ -53,10 +53,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia";
 
     vicinae.url = "github:vicinaehq/vicinae";
 
