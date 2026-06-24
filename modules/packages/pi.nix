054015a27ec9eb218fcca6bff6723fc7767672f6
Sami Reunanen <sami@reuski.dev>
2026-06-24 20:45:09 +0300

    pi via bunx

--- modules/packages/pi.nix
+++ modules/packages/pi.nix
@@ -1,6 +1,26 @@
 { ... }:
 {
   flake.overlays.pi = final: _prev: {
+    pi-coding-agent = final.writeShellApplication {
+      name = "pi";
+      runtimeInputs = [
+        final.bun
+        final.nodejs
+        final.ripgrep
+        final.fd
+      ];
+      text = ''
+        exec bun x @earendil-works/pi-coding-agent@latest "$@"
+      '';
+      meta = {
+        description = "Pi Coding Agent";
+        homepage = "https://pi.dev/";
+        license = final.lib.licenses.mit;
+        mainProgram = "pi";
+        platforms = final.lib.platforms.unix;
+      };
+    };
+
     pi-acp = final.writeShellApplication {
       name = "pi-acp";
       runtimeInputs = [
