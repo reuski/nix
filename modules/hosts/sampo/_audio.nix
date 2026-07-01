ed9de17757cc5ffc3fb73e496c82821c897f90e8
Sami Reunanen <sami@reuski.dev>
2026-07-01 19:51:10 +0300

    audio optim

--- modules/hosts/sampo/_audio.nix
+++ modules/hosts/sampo/_audio.nix
@@ -31,11 +31,6 @@
     extraConfig.pipewire."92-low-latency" = {
       "context.properties" = {
         "default.clock.rate" = 48000;
-        "default.clock.allowed-rates" = [
-          44100
-          48000
-          96000
-        ];
         "default.clock.quantum" = 64;
         "default.clock.min-quantum" = 32;
         "default.clock.max-quantum" = 2048;
