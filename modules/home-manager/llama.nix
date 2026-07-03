eedd1e261cc7034f40ff8f932a08f93dc37000c1
Sami Reunanen <sami@reuski.dev>
2026-07-03 09:08:36 +0300

    llama-server embetter

--- modules/home-manager/llama.nix
+++ modules/home-manager/llama.nix
@@ -34,26 +34,28 @@
           with pkgs;
           [
             cmake
+            ninja
             git
+            stdenv.cc
+            ccache
           ]
-          ++ lib.optionals (!isDarwin) [
-            gcc
-            cudatoolkit
-          ];
+          ++ lib.optionals (!isDarwin) [ cudatoolkit ];
         text = ''
           ${cudaEnv}
           llama_dir="$HOME/.local/src/llama.cpp"
           server="$llama_dir/build/bin/llama-server"
 
           build_llama() {
-            cmake -S "$llama_dir" -B "$llama_dir/build" \
+            cmake -S "$llama_dir" -B "$llama_dir/build" -G Ninja \
               -DBUILD_SHARED_LIBS=OFF \
+              -DCMAKE_BUILD_TYPE=Release \
               -DLLAMA_BUILD_TESTS=OFF \
               -DLLAMA_BUILD_EXAMPLES=OFF \
-              -DCMAKE_BUILD_TYPE=Release \
+              -DLLAMA_BUILD_APP=OFF \
+              -DGGML_BUILD_TESTS=OFF \
+              -DGGML_BUILD_EXAMPLES=OFF \
               ${backendFlags}
-            cmake --build "$llama_dir/build" --config Release \
-              -j "$(nproc 2>/dev/null || /usr/sbin/sysctl -n hw.logicalcpu)" --target llama-server
+            cmake --build "$llama_dir/build" --target llama-server
           }
 
           if [ ! -d "$llama_dir/.git" ]; then
@@ -61,14 +63,12 @@
             git clone https://github.com/ggml-org/llama.cpp.git "$llama_dir"
           fi
 
-          git -C "$llama_dir" fetch --prune origin master:refs/remotes/origin/master
+          git -C "$llama_dir" fetch origin
 
           if [ "$(git -C "$llama_dir" rev-parse HEAD)" != "$(git -C "$llama_dir" rev-parse origin/master)" ]; then
-            git -C "$llama_dir" merge --ff-only origin/master
-            git -C "$llama_dir" submodule update --init --recursive
+            git -C "$llama_dir" reset --hard origin/master
             build_llama
           elif [ ! -x "$server" ]; then
-            git -C "$llama_dir" submodule update --init --recursive
             build_llama
           fi
 
