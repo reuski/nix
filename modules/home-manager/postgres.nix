ddee3977bde801d33ebcc741ae5b8020e1ab0676
Sami Reunanen <sami@valohai.com>
2026-07-07 13:53:31 +0300

    postgres initdb wrapper

--- modules/home-manager/postgres.nix
+++ modules/home-manager/postgres.nix
@@ -10,6 +10,11 @@
             exec ${pkgs.lib.getExe' pkgs.postgresql_18 "postgres"} "$@"
           ''
         ))
+        (pkgs.lib.hiPrio (
+          pkgs.writeShellScriptBin "initdb" ''
+            exec ${pkgs.lib.getExe' pkgs.postgresql_18 "initdb"} "$@"
+          ''
+        ))
       ];
     };
 }
