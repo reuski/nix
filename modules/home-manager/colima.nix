a74b48dc3ac71f3a03b7e2453df12c6cf4a2fef8
Sami Reunanen <sami@reuski.dev>
2026-07-14 22:50:19 +0300

    fix colima

--- modules/home-manager/colima.nix
+++ modules/home-manager/colima.nix
@@ -2,7 +2,13 @@
 {
   flake.modules.homeManager.colima =
     { pkgs, ... }:
+    let
+      limaFull = pkgs.lima-full.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.llvmPackages.lld ];
+        NIX_CFLAGS_LINK = "-fuse-ld=${pkgs.llvmPackages.lld}/bin/ld64.lld";
+      });
+    in
     {
-      home.packages = [ pkgs.colima ];
+      home.packages = [ (pkgs.colima.override { lima-full = limaFull; }) ];
     };
 }
