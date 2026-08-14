95e4e6731d3266e402c21754339cb45889718686
Sami Reunanen <sami@reuski.dev>
2026-08-14 09:18:05 +0300

    fix hass warning

--- modules/nixos/hass.nix
+++ modules/nixos/hass.nix
@@ -16,13 +16,6 @@
         frontend:
           themes: !include_dir_merge_named themes
 
-        http:
-          server_host: 127.0.0.1
-          use_x_forwarded_for: true
-          trusted_proxies:
-            - 127.0.0.1
-            - "::1"
-
         automation: !include automations.yaml
         script: !include scripts.yaml
         scene: !include scenes.yaml
