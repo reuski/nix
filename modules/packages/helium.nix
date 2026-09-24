f9dd763bbf1c158e9b0e9a7c98c6e7b84cc35093
reuski <631912+reuski@users.noreply.github.com>
2026-09-24 12:10:08 +0000

    chore: update flake inputs

--- modules/packages/helium.nix
+++ modules/packages/helium.nix
@@ -48,11 +48,11 @@
 
       stdenvNoCC.mkDerivation (finalAttrs: {
         pname = "helium-browser";
-        version = "0.17.2.1";
+        version = "0.18.1.1";
 
         src = fetchurl {
           url = "https://github.com/imputnet/helium-linux/releases/download/${finalAttrs.version}/helium-${finalAttrs.version}-x86_64_linux.tar.xz";
-          hash = "sha256-KmOd9U49BfQTz7tGIqTRpoWEsx2lp6r1juPNg8fD4pk=";
+          hash = "sha256-n001I57qGLKQhGIhh0JlrCqGN63/lU32n973fWsVBCw=";
         };
 
         sourceRoot = "helium-${finalAttrs.version}-x86_64_linux";
